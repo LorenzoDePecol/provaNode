d66f25baabcfdab612eb24c6072f2d7ab95f9495
De Pecol Lorenzo <depecol.lorenzo@osdbsesto>
2021-01-28 13:25:55 +0100

    utilizzo oggetto path

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ var hostname = "127.0.0.1";
 var port = 3000;
 var nome = "NODEDEPECOL";
 
+var path = require("path");
+
 apiServer.listen(port, hostname, () => {
     console.log("server running at http://%s:%d/", hostname, port);
 });
@@ -17,4 +19,9 @@ apiServer.get("/nome", (req, res) =>{
 
 apiServer.get("/", function(req, res){
     res.send("sei in home");
+});
+
+apiServer.get("/index", (req, res) => {
+    //res.sendFile("C:\\Users\\depecol.lorenzo\\Desktop\\proba\\provaNode\\index.html");
+    res.sendFile(path.join(__dirname + "/index.html"));
 });
