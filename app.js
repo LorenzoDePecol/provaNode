1afbacf2e8945fc6f8ddf421335798c80ad35b33
De Pecol Lorenzo <depecol.lorenzo@osdbsesto>
2021-01-28 13:02:41 +0100

    fattorizzazione a node

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
-var http = require("http");
 var express = require("express");
+var apiServer = express();
+
 
 var hostname = "127.0.0.1";
 var port = 3000;
 
-var server = http.createServer((req, res) => {
-    console.log("req:", req);
-    res.statusCode = 200;
-    res.setHeader("content-Type", "text/plain");
-    res.end("Ciao belloccio!");
+apiServer.listen(port, hostname, () => {
+    console.log("server running at http://%s:%d/", hostname, port);
 });
 
-server.listen(port, hostname, () => {
-    console.log("server running at http://%s:%d/", hostname, port);
+apiServer.get("nome", (req, res) =>{
+    console.log("ho ricevuto un get su nome");
+    res.statusCode = 200;
+    res.setHeader("content-Type", "text/plain");
+    res.end("Ciao sono il server Node");
 });
